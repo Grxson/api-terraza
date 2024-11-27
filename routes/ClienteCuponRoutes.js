85cdaa7272e948395e607503e48c75ef7d373031
Grxson <gael.grxson@gmail.com>
2024-11-26 21:03:13 -0600

    protección de rutas

--- routes/ClienteCuponRoutes.js
+++ routes/ClienteCuponRoutes.js
@@ -5,6 +5,6 @@ import protegerRuta from "../middleware/protegerRuta.js";
 const router = express.Router()
 
 // Ruta para obtener los cupones de un cliente
-router.get('/:_id', getCuponesDeCliente);
+router.get('/:_id', protegerRuta, getCuponesDeCliente);
 
 export default router;
